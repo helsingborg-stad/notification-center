b689eaa1edb60e8610d830046679f0f1349f1fb5
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-02-06 14:24:29 +0100

    New dropdown markup

--- templates/dropdown.php
+++ templates/dropdown.php
@@ -1,19 +1,29 @@
 <div class="notification-center">
-	<a href="#" class="notification-center__toggle">
-		<?php echo $icon; ?>
+	<a href="#" class="notification-center__toggle" data-unseen="<?php echo $unseen; ?>">
+		<?php echo $toggleIcon; ?>
 	</a>
 
 	<div class="notification-center__dropdown">
+		<div class="notification-center__header">
+			<?php _e('Notifications', 'notification-center'); ?>
+		</div>
 		<ul>
 			<?php
 			if (!empty($notifications)):
 			foreach ($notifications as $key => $notification): ?>
-				<li class="notification-center__item notification-center__item--read">
-					<?php echo \NotificationCenter\Notification::buildMessage($notification->entity_type, $notification->entity_id, $notification->sender_id); ?>
+				<li class="notification-center__item <?php echo !$notification->status ? 'notification-center__item--unseen' : ''; ?>"  data-notification-id="<?php echo $notification->ID; ?>">
+					<a href="<?php echo \NotificationCenter\Notification::notificationUrl($notification->entity_type, $notification->entity_id); ?>">
+						<div class="notification-center__message">
+							<?php echo \NotificationCenter\Notification::buildMessage($notification->entity_type, $notification->entity_id, $notification->sender_id); ?>
+						</div>
+						<div class="notification-center__time">
+							<i class="pricon pricon-clock"></i> <?php echo human_time_diff(strtotime($notification->created), current_time('timestamp')) . ' ' . __('sedan', 'notification-center'); ?>
+						</div>
+					</a>
 				</li>
-			<?php endforeach;
+			<?php
+			endforeach;
 			endif; ?>
-			<li class="notification-center__load-more"><p>Load more</p></li>
 		</ul>
 	</div>
 </div>
