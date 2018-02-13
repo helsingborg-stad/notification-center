4132950b6d535183392cba247165d138d5a935da
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-02-13 15:03:56 +0100

    Email summary option

--- acf-fields/php/notification_options.php
+++ acf-fields/php/notification_options.php
@@ -3,27 +3,26 @@
 if (function_exists('acf_add_local_field_group')) {
     acf_add_local_field_group(array(
     'key' => 'group_5a7dc01cb8cd6',
-    'title' => 'Notification center',
+    'title' => __('Notification center', 'notification-center'),
     'fields' => array(
         0 => array(
-            'key' => 'field_5a7dc17460fff',
-            'label' => __('Follow', 'notification-center'),
-            'name' => 'notification_post_types',
-            'type' => 'posttype_select',
-            'instructions' => __('Enable follow functionality for selected post types.', 'notification-center'),
+            'key' => 'field_5a82ec34ea342',
+            'label' => __('Daily email summary', 'notification-center'),
+            'name' => 'notification_email_summary',
+            'type' => 'true_false',
+            'instructions' => '',
             'required' => 0,
             'conditional_logic' => 0,
             'wrapper' => array(
-                'width' => '50',
+                'width' => '',
                 'class' => '',
                 'id' => '',
             ),
-            'default_value' => '',
-            'allow_null' => 0,
-            'multiple' => 1,
-            'placeholder' => '',
-            'disabled' => 0,
-            'readonly' => 0,
+            'message' => __('Send daily email summary', 'notification-center'),
+            'default_value' => 0,
+            'ui' => 0,
+            'ui_on_text' => '',
+            'ui_off_text' => '',
         ),
     ),
     'location' => array(
--- source/php/Admin/Options.php
+++ source/php/Admin/Options.php
@@ -9,7 +9,7 @@ class Options
         if (function_exists('acf_add_options_sub_page')) {
             acf_add_options_sub_page(array(
                 'page_title'    => _x('Notification center settings', 'Notification center', 'notification-center'),
-                'menu_title'    => _x('Notification center', 'Notification center', 'notification-center'),
+                'menu_title'    => _x('Notifications', 'Notification center', 'notification-center'),
                 'menu_slug'     => 'notification-center-options',
                 'parent_slug'   => 'options-general.php',
                 'capability'    => 'manage_options'
