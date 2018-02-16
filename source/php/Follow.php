45705ffd02dc6821bcdb5bbd6d711a9be3cda244
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-02-16 11:39:08 +0100

    Changed label Following to Unfollow

--- source/php/Follow.php
+++ source/php/Follow.php
@@ -75,7 +75,7 @@ class Follow
         $user = wp_get_current_user();
         $followers = get_post_meta($post->ID, 'post_followers', true);
         $isFollowing = (is_array($followers) && in_array($user->ID, array_keys(array_filter($followers)))) ? 1 : 0;
-        echo '<li><a href="#" class="follow-button ' . ($isFollowing ? 'follow-button--following' : '') . ' " data-post-id="' . $post->ID . '"><i class="pricon ' . ($isFollowing ? 'pricon-star' : 'pricon-star-o') . '"></i> <span class="follow-button__text">' . ($isFollowing ? __('Following', 'notification-center') : __('Follow', 'notification-center')) . '</span></a></li>';
+        echo '<li><a href="#" class="follow-button ' . ($isFollowing ? 'follow-button--following' : '') . ' " data-post-id="' . $post->ID . '"><i class="pricon ' . ($isFollowing ? 'pricon-star' : 'pricon-star-o') . '"></i> <span class="follow-button__text">' . ($isFollowing ? __('Unfollow', 'notification-center') : __('Follow', 'notification-center')) . '</span></a></li>';
     }
 
     /**
@@ -95,7 +95,7 @@ class Follow
         $user = wp_get_current_user();
         $followers = get_post_meta($post->ID, 'post_followers', true);
         $isFollowing = (is_array($followers) && in_array($user->ID, array_keys(array_filter($followers)))) ? 1 : 0;
-        $items[] = '<span><a href="#" class="follow-button ' . ($isFollowing ? 'follow-button--following' : '') . ' " data-post-id="' . $post->ID . '"><i class="pricon ' . ($isFollowing ? 'pricon-star' : 'pricon-star-o') . '"></i> <span class="follow-button__text">' . ($isFollowing ? __('Following', 'notification-center') : __('Follow', 'notification-center')) . '</span></a></span>';
+        $items[] = '<span><a href="#" class="follow-button ' . ($isFollowing ? 'follow-button--following' : '') . ' " data-post-id="' . $post->ID . '"><i class="pricon ' . ($isFollowing ? 'pricon-star' : 'pricon-star-o') . '"></i> <span class="follow-button__text">' . ($isFollowing ? __('Unfollow', 'notification-center') : __('Follow', 'notification-center')) . '</span></a></span>';
 
         return $items;
     }
