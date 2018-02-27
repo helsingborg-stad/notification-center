1e2e64e6e06f1234ba6c8fcfd2559cbe9fda1260
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-02-27 09:36:11 +0100

    Show options on parent blog

--- source/php/Admin/Options.php
+++ source/php/Admin/Options.php
@@ -6,7 +6,7 @@ class Options
 {
     public function __construct()
     {
-        if (function_exists('acf_add_options_sub_page')) {
+        if (function_exists('acf_add_options_sub_page') && get_current_blog_id() == 1) {
             acf_add_options_sub_page(array(
                 'page_title'    => _x('Notification center settings', 'Notification center', 'notification-center'),
                 'menu_title'    => _x('Notifications', 'Notification center', 'notification-center'),
