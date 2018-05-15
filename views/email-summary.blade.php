f2bf01f59612ff070770bfe39da318119fd77204
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-05-15 12:57:21 +0200

    Changed to tables to work with Outlook

--- views/email-summary.blade.php
+++ views/email-summary.blade.php
@@ -1,16 +1,18 @@
 <h2>{{ $heading }}</h2>
-<ul style="list-style-type:none;padding:0;">
+<table style="font-family:Helvetica,Arial,sans-serif;" cellspacing="10" cellpadding="10" border="0">
     @foreach ($notifications as $notification)
-        <li style="padding:15px;background:#e6e6e6;border-radius:3px;margin-bottom:15px;">
-            <small style="color:#888;">
-                {{ mysql2date('j F', $notification->created, true) . ', ' . mysql2date('H:i', $notification->created, true) }}
-            </small>
-            <p>{!! \NotificationCenter\Helper\Message::buildMessage($notification->entity_type, $notification->entity_id, $notification->sender_id, $notification->count, $notification->blog_id) !!}</p>
-            <a href="{{ \NotificationCenter\Helper\Message::notificationUrl($notification->entity_type, $notification->entity_id, $notification->blog_id) }}" target="_blank" style="color: #ffffff; font-size:12px; text-decoration: none; border-radius: 3px; background-color: #a84c98; border: 7px solid #a84c98; display: inline-block;">
-                <?php _e('Show', 'notification-center'); ?> {{ strtolower($entityTypes[$notification->entity_type]['label']) }}
-            </a>
-        </li>
+        <tr>
+            <td bgcolor="#e6e6e6" style="border-radius:3px;">
+                <small style="color:#888;">
+                    {{ mysql2date('j F', $notification->created, true) . ', ' . mysql2date('H:i', $notification->created, true) }}
+                </small>
+                <p>{!! \NotificationCenter\Helper\Message::buildMessage($notification->entity_type, $notification->entity_id, $notification->sender_id, $notification->count, $notification->blog_id) !!}</p>
+                <a href="{{ \NotificationCenter\Helper\Message::notificationUrl($notification->entity_type, $notification->entity_id, $notification->blog_id) }}" target="_blank" style="color: #fff; font-size:12px; text-decoration: none; border-radius: 3px; background-color: #a84c98; border: 7px solid #a84c98; display: inline-block;">
+                   <span style="color:#fff"><?php _e('Show', 'notification-center'); ?> {{ strtolower($entityTypes[$notification->entity_type]['label']) }}</span>
+                </a>
+            </td>
+        </tr>
     @endforeach
-</ul>
+</table>
 <br>
 {!! $footer !!}
